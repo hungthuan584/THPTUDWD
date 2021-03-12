aa632fa9f6047a4f6edb97db22e0d4ee309c45e8
Nguyễn Hùng Thuận <74907065+hungthuan584@users.noreply.github.com>
2021-03-11 16:42:02 -0800

    songuyenduong

---
+++ Chuong1/SoNguyenDuong.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chuong1
+{
+    class SoNguyenDuong
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            int t = -110;
+            do
+            {
+                System.Console.WriteLine("Nhap vao so nguyen: ");
+                a = int.Parse(System.Console.ReadLine());
+                t += a;
+            }
+            while (t < 0);
+            System.Console.WriteLine("Da nhan duoc so nguyen duong");
+            Console.ReadLine();
+        }
+    }
+}
